e6ac6c1c50692b243037f5ab83077980e8e45516
ZDavy <2217858390@qq.com>
2022-08-15 00:40:39 +0800

    新增自动化获取书签和识别PDF页偏移功能

--- pdf_add_bookmark_semi.py
+++ pdf_add_bookmark_semi.py
@@ -1,13 +1,127 @@
 import sys
-import format_bookmark as fbk
 from pikepdf import Pdf, OutlineItem
 from colorama import init 
 import os
 from yaml import safe_load
+from pywinauto.application import Application
+from time import sleep
+from chardet import detect
+import subprocess
+import re
+from warnings import simplefilter
+
+
+
+def auto_fetch_bookmark(file_name,ssid):
+    #ok TODO file_name解析
+    path = "书签获取小工具2015.05.05【晴天软件】.exe"
+    app = Application(backend='win32').start(path)
+    # 连接软件的主窗口
+    dlg_spec = app.window(title_re='书签获取小工具2015.05.05  【晴天软件】*', class_name_re='WTWindow*')
+    # 设置焦点，使其处于活动状态
+    dlg_spec.set_focus()
+    dlg_spec['Edit2'].set_edit_text(ssid)
+    # UIA 一直无dlg_spec['Edit'].window_text()
+    #ok TODO 注意没书签超时情况
+    while dlg_spec['Edit'].window_text()=='' or dlg_spec['Edit'].window_text()[:5]=='【话痨：】':
+        pass
+    if dlg_spec['Edit'].window_text()[:11]=='没有查询到此SS的书签':
+        print("\n没有查询到此SS的书签,请手动获取书签\n")
+        print("\033[0;32;40m按Enter键退出！\033[0m\n")
+        input()
+        sys.exit()
+
+    sleep(1)
+    # 获取文本框数据
+    with open(file_name,'w+',encoding='utf-8') as f:
+        f.write(dlg_spec['Edit'].window_text())
+    
+    return dlg_spec
+    # 关闭窗口
+    # dlg_spec.close()    
+    
+
+# def parse_params():
+    #cancel TODO 参数解析 getopt
+
+
+
+# pdf为打开的pdf对象
+def recognize_page_offset(pdf):
+    #ok TODO 页偏移识别
+    content_page=page_offset=None
+    for index in range(len(pdf.pages)):
+        if pdf.pages[index].label =='i':
+            # 目录页码
+            content_page=index+1
+        elif pdf.pages[index].label =='1':
+            # 页偏移
+            page_offset=index
+            break
+        elif index>50:
+            # 非标准PDF,无法识别页偏移
+            break
+    return content_page, page_offset
+
+
+def format_bookmark():
+    config_path=sys.argv[0]
+    txt_filename=sys.argv[1].replace('.pdf','.txt')
+    page_offset_number=sys.argv[2]
+    if len(sys.argv)==4 :
+        directory_number=sys.argv[3]
+#    print('打开config.yaml','\n')
+#    input('调试\n')
+    with open(config_path+'\\Config\\config.yaml','r',encoding='utf-8') as f:
+#        print(config_path+'\\Config\\config.yaml'+'\n')
+#        input('调试\n')
+        config=safe_load(f)                                      
+#        检测编码GB18030、utf-8
+        with open(txt_filename,'rb') as f:
+            data = f.read()
+            file_encoding = detect(data)['encoding']
+            with open(str(txt_filename), 'r', encoding=file_encoding, errors = 'ignore') as f:
+                str_list = f.read()
+#                书签格式化
+                for i in range(len(config['rules'])) :
+                    for j in range(len(config['rules'][i]['rule'])):
+                        regex_search=config['rules'][i]['rule'][j]['regex_search']
+                        regex_repalce=config['rules'][i]['rule'][j]['regex_repalce']
+                        if config['rules'][i]['rule'][j]['loaded'] and regex_search!=None and regex_repalce!=None:
+                            regex_compiled = re.compile(regex_search, re.M)
+                            str_list_re = regex_compiled.sub(regex_repalce, str_list)
+                            str_list = str_list_re
+#                    加页偏移，后面有页偏移是动态变量加不进yaml
+                regex_page_offset = re.compile(r'(\d+)$', re.M)
+                str_list = regex_page_offset.sub(r'\1\t+'+str(page_offset_number),str_list)
+#                    路径保留空格
+                filename=str(txt_filename).split('\\')[-1]
+                path_name=str(txt_filename).split(filename)[0]
+#                    文件名去空格？
+                add_bookmark_filename = path_name+filename.replace(' ', '').split(
+                    '.txt')[0]+r"(Bookmark)"+"."+filename.replace(' ', '').split('.')[-1]
+                with open(add_bookmark_filename, 'w+', encoding='utf-8-sig') as fw:
+                    if len(sys.argv) == 4:
+                        if config['options']['bookmark_options']['first_letter_lower']:
+                            fw.write('目录\t'+str(directory_number)+'\n'+str_list.lower())
+                        else:
+                            fw.write('目录\t'+str(directory_number)+'\n'+str_list)
+                    elif len(sys.argv)==3:
+                        if config['options']['bookmark_options']['first_letter_lower']:
+                            fw.write(str_list.lower())
+                        else:
+                            fw.write(str_list)
+#               初次格式化完书签，再次使用Notepad3编辑修正
+                print("\n书签部分格式化完成，请在打开的编辑器修正书签文件，并\033[0;31;40m修正完后关闭编辑器！\033[0m\n")
+                if config['options']['global_options']['enable_editor']:
+                    editor_path=(config['options']['global_options']['editor_path'] if ':'  in config['options']['global_options']['editor_path'] else config_path+config['options']['global_options']['editor_path'])
+                    # print(editor_path,'\n')
+                    subprocess.run([editor_path,add_bookmark_filename])
+
 
 def pike_add_bookmark():
     config_path=sys.argv[0]
-    txt_filename=sys.argv[1]
+    txt_filename=sys.argv[1].replace('.pdf','.txt')
     pdf_name = str(txt_filename).split('.txt')[0]+"."+"pdf"
     filename=str(txt_filename).split('\\')[-1]
     path_name=str(txt_filename).split(filename)[0]
@@ -48,42 +162,83 @@ def pike_add_bookmark():
                             L3_item.children.append(L4_item)
             pdf.save(pdf_name)
 
-if __name__ == "__main__":
-#    方便双击使用
+
+def main():
     init()
+    simplefilter('ignore', category=UserWarning)
+    os.system("title " + "pdf_add_bookmark_semi@DavyZhou")
+    print("代码更新请访问："+"\033[0;32;40m https://github.com/Davy-Zhou/pdf_add_bookmark_semi \033[0m"+"\n")
     if len(sys.argv)==1:
-        print("\n请按格式输入：  \033[0;32;40m书签内容文件名(务必加双引号) 正文页偏移 目录页码(参数可选)\033[0m\n")
-        print("使用示例:  \033[0;32;40m\"C语言大学实用教程第4版.txt\" 10 7 \033[0m\n\n")
+        # 这段代码方便双击使用
+        print("新增自动化获取书签和识别PDF页偏移功能，直接拖入需要加书签的pdf即可,pdf文件名务必加ssid号还有双引号\n")
+        print("使用示例:  \033[0;32;40m\"C语言大学实用教程第4版_14133899.pdf\" \033[0m\n")
+        print("无法自动获取书签请按以下格式输入：  \033[0;32;40m书签txt文件名(务必加双引号) 正文页偏移 目录页码(参数可选)\033[0m\n")
+        print("使用示例:  \033[0;32;40m\"C语言大学实用教程第4版.txt\" 10 7 \033[0m\n")
         params =input("请输入： ").split('\"')
-#        print(params,'\n')
-#        input('调试\n')
+        # 拿到代码文件路径，得配置文件绝对路径
         py_dirname, py_name = os.path.split(os.path.abspath(sys.argv[0]))
         sys.argv.clear()
         sys.argv.extend([py_dirname])
-        sys.argv.extend([params[1]])
-        sys.argv.extend(params[-1].split())
-#        print(sys.argv,'\n')
-#        input('调试\n')
+        # 判断是否自动获取书签
+        if params[1].lower().endswith('pdf',len(params[1])-3,len(params[1])):
+            sys.argv.extend([params[1]])
+        elif params[1].lower().endswith('txt',len(params[1])-3,len(params[1])):
+            # 兼容命令行模式
+            sys.argv.extend([params[1]])
+            sys.argv.extend(params[-1].split())
+        else:
+            print("\033[0;31;40m 拖入文件必须是txt或pdf文件 \033[0m\n\n")
+            print("\033[0;32;40m按Enter键退出！\033[0m\n")
+            input()
+            sys.exit()
+
     else:
+        # 适配命令行模式
         py_dirname, py_name = os.path.split(os.path.abspath(sys.argv[0]))
         sys.argv[0]=py_dirname
-    filename=str(sys.argv[1]).split('\\')[-1].replace('txt','pdf')
-#    print(filename,'\n')
-#    input('调试\n')
-    if len(sys.argv)<3 :
+    pdf_filename=str(sys.argv[1]).split('\\')[-1].replace('txt','pdf')
+
+    if len(sys.argv)<3 and sys.argv[1].lower().endswith('txt',len(sys.argv[1])-3,len(sys.argv[1])) :
        print("\n错误：参数不足\n使用示例:  \"C语言大学实用教程第4版.txt\" 17 12\n或者:  \"C语言大学实用教程第4版.txt\" 17\n")
        sys.exit()
-#    print('进入format_bookmark','\n')
-#    input('调试\n')
-    fbk.format_bookmark()
-    print("是否给《\033[0;32;40m"+filename+"\033[0m》加书签(Y/N)?\n")
-    is_add_Bookmark=input()
-    if is_add_Bookmark.lower() != 'y':
+
+    # 解析ssid
+    if sys.argv[1].lower().endswith('pdf',len(sys.argv[1])-3,len(sys.argv[1])):
+        #ok TODO自动获取书签，并返回主窗口,前面命令行参数都得改
+        bookmark_filename=str(sys.argv[1]).replace('pdf','txt')
+        ssid=re.findall('\d{8}',str(sys.argv[1]).split('\\')[-1])
+        if ssid==[]:
+            print("\n 无法识别SSID,请手动添加SSID到pdf文件名上面\n")
+            print("\033[0;32;40m按Enter键退出！\033[0m\n")
+            input()
+            sys.exit()
+        dlg_spec=auto_fetch_bookmark(bookmark_filename,ssid[0])
+        dlg_spec.close()
+        sleep(1)
+        with Pdf.open(str(sys.argv[1]),allow_overwriting_input=True) as pdf:
+            content_page, page_offset=recognize_page_offset(pdf)
+            if page_offset==None or page_offset==0:
+                print("\n 无法识别页偏移,请使用第二种模式手动计算页偏移加书签\n")
+                print("\033[0;32;40m按Enter键退出！\033[0m\n")
+                input()
+                sys.exit()
+            sys.argv.append(page_offset)
+            if content_page!=None:
+                sys.argv.append(content_page)
+
+    format_bookmark()
+    print("是否给《\033[0;32;40m"+pdf_filename+"\033[0m》加书签(Y/N)?\n")
+    is_add_bookmark=input()
+    if is_add_bookmark.lower() != 'y':
         print("\n仅完成书签部分格式化！\n")
         print("\033[0;32;40m按Enter键退出！\033[0m\n")
         input()
         sys.exit()
     pike_add_bookmark()
-    print("\n\033[0;32;40m"+filename+"\033[0m 加书签完成！\n\n")
+    print("\n\033[0;32;40m"+pdf_filename+"\033[0m 加书签完成！\n\n")
     print("\033[0;32;40m按Enter键退出！\033[0m\n")
     input()
+
+
+if __name__ == "__main__":
+    main()
